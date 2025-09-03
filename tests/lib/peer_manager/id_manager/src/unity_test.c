fca0a5322c9c3ecadf0ebb9e754e21fef015f503
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2025-09-03 14:59:47 +0200

    tests: id_manager: Adapt to newer cmock version
    Add explicit calls to *Expect*() when stubbing.
    
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

--- tests/lib/peer_manager/id_manager/src/unity_test.c
+++ tests/lib/peer_manager/id_manager/src/unity_test.c
@@ -151,6 +151,7 @@ void test_im_ble_evt_handler(void)
 	/* Previously bonded. */
 	__cmock_pds_peer_data_iterate_prepare_Expect();
 	__cmock_pds_peer_data_iterate_StubWithCallback(pds_peer_data_iterate_stub_im_evt_handler);
+	__cmock_pds_peer_data_iterate_ExpectAnyArgsAndReturn(true);
 
 	im_ble_evt_handler(&ble_evt);
 	TEST_ASSERT_EQUAL(m_peer_id, m_connections[m_conn_handle].peer_id);
@@ -354,8 +355,8 @@ void test_im_peer_id_get_by_master_id(void)
 	__cmock_pds_peer_data_iterate_prepare_Expect();
 	__cmock_pds_peer_data_iterate_StubWithCallback(
 		pds_peer_data_iterate_stub_im_peer_id_get_by_master_id);
-	__cmock_pds_peer_data_iterate_StubWithCallback(
-		pds_peer_data_iterate_stub_im_peer_id_get_by_master_id);
+	__cmock_pds_peer_data_iterate_ExpectAnyArgsAndReturn(true);
+	__cmock_pds_peer_data_iterate_ExpectAnyArgsAndReturn(true);
 
 	master_id.ediv = m_ediv;
 	memcpy(master_id.rand, m_rand, BLE_GAP_SEC_RAND_LEN);
@@ -633,6 +634,9 @@ void test_im_whitelist_get(void)
 	 */
 
 	__cmock_pds_peer_data_read_StubWithCallback(pds_peer_data_read_stub_whitelist_get);
+	__cmock_pds_peer_data_read_ExpectAnyArgsAndReturn(NRF_SUCCESS);
+	__cmock_pds_peer_data_read_ExpectAnyArgsAndReturn(NRF_SUCCESS);
+	__cmock_pds_peer_data_read_ExpectAnyArgsAndReturn(NRF_SUCCESS);
 
 	ret = im_whitelist_get((ble_gap_addr_t *)addrs, &wlisted_peer_cnt, NULL,
 			       NULL); /* Don't fetch IRKs */
@@ -674,14 +678,19 @@ void test_im_whitelist_set(void)
 	uint32_t const peer_cnt = (sizeof(peers) / sizeof(pm_peer_id_t));
 
 	__cmock_pds_peer_data_read_StubWithCallback(pds_peer_data_read_stub);
+	__cmock_pds_peer_data_read_ExpectAnyArgsAndReturn(NRF_SUCCESS);
 
 	/* Peer is not valid. */
 	ret = im_whitelist_set(peers, peer_cnt);
 	TEST_ASSERT_EQUAL(NRF_ERROR_NOT_FOUND, ret);
 
+	__cmock_pds_peer_data_read_ExpectAnyArgsAndReturn(NRF_SUCCESS);
+
 	/* Peer is valid, but there is no data in flash. */
 	ret = im_whitelist_set(peers, peer_cnt);
 	TEST_ASSERT_EQUAL(NRF_ERROR_NOT_FOUND, ret);
+
+	__cmock_pds_peer_data_read_ExpectAnyArgsAndReturn(NRF_SUCCESS);
 
 	/* Peer data was found but the peer address can not be whitelisted. */
 	ret = im_whitelist_set(peers, peer_cnt);
@@ -689,6 +698,9 @@ void test_im_whitelist_set(void)
 
 	__cmock_sd_ble_gap_whitelist_set_ExpectAndReturn(NULL, peer_cnt, NRF_SUCCESS);
 	__cmock_sd_ble_gap_whitelist_set_IgnoreArg_pp_wl_addrs();
+	__cmock_pds_peer_data_read_ExpectAnyArgsAndReturn(NRF_SUCCESS);
+	__cmock_pds_peer_data_read_ExpectAnyArgsAndReturn(NRF_SUCCESS);
+	__cmock_pds_peer_data_read_ExpectAnyArgsAndReturn(NRF_SUCCESS);
 
 	ret = im_whitelist_set(peers, peer_cnt);
 	TEST_ASSERT_EQUAL(NRF_SUCCESS, ret);
@@ -748,6 +760,7 @@ void test_im_device_identities_list_set(void)
 	}
 	__cmock_sd_ble_gap_device_identities_set_StubWithCallback(
 		sd_ble_gap_device_identities_set_stub);
+	__cmock_sd_ble_gap_device_identities_set_ExpectAnyArgsAndReturn(NRF_SUCCESS);
 	ret = im_device_identities_list_set(peers, 3);
 	TEST_ASSERT_EQUAL(NRF_SUCCESS, ret);
 }
@@ -800,6 +813,7 @@ void test_im_address_resolve(void)
 		expected_ecb_hal_data.ciphertext[SOC_ECB_KEY_LENGTH - 1 - i] = addr_array[i];
 	}
 	__cmock_sd_ecb_block_encrypt_StubWithCallback(callback);
+	__cmock_sd_ecb_block_encrypt_ExpectAnyArgsAndReturn(NRF_SUCCESS);
 	n_ecb_calls = 0;
 	TEST_ASSERT_EQUAL_UINT(true, im_address_resolve(&addr, &irk));
 }
